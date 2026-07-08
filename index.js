07d7c211ba643e90217fd866a1858c0fbd961cc8
afaque-js <shaikhafaque66@gmail.com>
2026-07-09 02:05:28 +0530

    Edited Login Page, added Signup buttom, added signup and login logic

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
-import process from "node:process";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
-const PORT = Number(process.env.PORT) || 5000;
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Backend server is running" });
-});
+const dbURI =
+  "mongodb+srv://shaikhafaque66:As7447675144%40@learning.nmnjlcm.mongodb.net/?appName=Learning";
 
-const startServer = async () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
   });
 
-  try {
-    await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/admin-dashboard",
-    );
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  }
-};
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
 
-startServer();
+const User = mongoose.model("User", userSchema);
+
+app.post("/signup", async (req, res) => {
+  try {
+    const newUser = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Signup Failed" + error.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (!existingUser) {
+      return res.status(400).json({ error: "Invalid Email" });
+    }
+
+    if (existingUser.password !== req.body.password) {
+      return res.status(400).json({ error: "Invalid Password" });
+    }
+
+    res.status(200).json({ message: "Login Successful" });
+  } catch (err) {
+    res.status(500).json({ error: "Login Failed" + err.message });
+  }
+});
+
+app.listen(5000, () => {
+  console.log(`Server running on http://localhost:5000`);
+});
